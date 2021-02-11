5ec6f98198ba29db866b8dec00bf709752a91fce
Jochen Leeder <jochenleeder@Jochens-MBP.fritz.box>
2021-02-11 15:25:04 +0100

    frist

---
+++ textausgabe.py
@@ -0,0 +1,8 @@
+print ('"Willkommen im Python_Kurs!"')
+print ("")
+print ('das ist ein Programm mit dem die Autos in eine Datenbank aufnehmen kannst')
+print ("HALLO MEIN NAME IST CARL")
+print ("5+3")
+print (5 + 3)
+5 + 3
+((45 + 34) + (34 * 3)) * 2
