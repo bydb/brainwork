2cb0a92da84ae9f4c114b5d3a175d4c4c81c3fb2
Jochen Leeder <jochenleeder@Jochens-MBP.fritz.box>
2021-02-11 15:29:50 +0100

    erste anpassung

--- textausgabe.py
+++ textausgabe.py
@@ -1,8 +1,3 @@
 print ('"Willkommen im Python_Kurs!"')
-print ("")
-print ('das ist ein Programm mit dem die Autos in eine Datenbank aufnehmen kannst')
-print ("HALLO MEIN NAME IST CARL")
-print ("5+3")
-print (5 + 3)
-5 + 3
-((45 + 34) + (34 * 3)) * 2
+print ("Rechnen in Python")
+print (4 + 6)
